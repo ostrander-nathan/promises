18e657b2a5e75db0a6c57ebf1d5d90d47f2d5a9a
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-04-27 11:41:13 -0500

    clean & readme

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Promises
 
-### TO RUN:
-* cd lib
-    - bower install
-    - npm install
-* http-server
+### Description: 
+
+* Introduction into Promises / Ajax calls
+
+### Final Result:
+> Displays results from Ajax calls to JSON as promise returns
+
+### Take-home Lesson:
+
+> Notes in code show data results from calls passing to promises 
+
+
+### How to run (Node must be installed on your machine):
+```
+git clone https://github.com/ostrander-nathan/promises
+npm install http-server -g
+http-server -p 8080
+```
+
+
+### Contributors:
+[Nathan Ostrander](https://github.com/ostrander-nathan)
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,109 +1,65 @@
 "use strict";
 // wrapper for jquery is ready to fire
 $(document).ready(function() {
-  console.log("jquery is ready");
-  var contentEl = $("#all-my-songs");
-  var songs = [];
+    // console.log("jquery is ready");
+    var contentEl = $("#all-my-songs");
+    var songs = [];
 
-function getSongs(){
-  return new Promise ((resolve, reject) => {
-    $.ajax({
-      url:"../songs.json"
-    }).done(function(data){
-      resolve(data);
-    }).fail(function(xhr, status, error){
-      reject(error);
-    });
+    function getSongs() {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "../songs.json"
+            }).done(function(data) {
+                resolve(data);
+            }).fail(function(xhr, status, error) {
+                reject(error);
+            });
 
-  });
-}
+        });
+    }
 
-function getSongs2(resultOfFirstAjax){
-  return new Promise ((resolve, reject) => {
-    $.ajax({
-      url:"../songs2.json"
-    }).done(function(data2){
-      songs = resultOfFirstAjax.songs;
-      resolve(data2);
-    }).fail(function(xhr2, status2, error2){
-      reject(error2);
-    });
+    function getSongs2(resultOfFirstAjax) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "../songs2.json"
+            }).done(function(data2) {
+                songs = resultOfFirstAjax.songs;
+                resolve(data2);
+            }).fail(function(xhr2, status2, error2) {
+                reject(error2);
+            });
 
-  });
-}
+        });
+    }
 
-getSongs().then(function(dataPass){ //dataPass same as data on line 12
-  console.log("dataPass",dataPass );
-  console.log("song 1st then",songs );
-  return getSongs2(dataPass);
-}).then(function(dataPass2){
-  console.log("song 2nd then", songs); // 3songs from 1st ajax
-  console.log("dataPass2",dataPass2 ); // 2nd json obj
-var songData = "";
+    getSongs().then(function(dataPass) { //dataPass same as data on line 12
+        // console.log("dataPass",dataPass );
+        // console.log("song 1st then",songs );
+        return getSongs2(dataPass);
+    }).then(function(dataPass2) {
+        // console.log("song 2nd then", songs); // 3songs from 1st ajax
+        // console.log("dataPass2",dataPass2 ); // 2nd json obj
+        var songData = "";
         var currentSong;
-        
-        dataPass2.songs.forEach(function(song){
-          songs.push(song);
+
+        dataPass2.songs.forEach(function(song) {
+            songs.push(song);
         });
 
-        console.log("songs", songs);
-        
         for (var i = 0; i < songs.length; i++) {
-          currentSong = songs[i];
+            currentSong = songs[i];
 
-          songData += "<div class='song-block'>";
+            songData += "<div class='song-block'>";
             songData += "<h1>" + currentSong.title + "</h1>";
             songData += "<div class='artist'>Performed by ";
-              songData += currentSong.artist;
+            songData += currentSong.artist;
             songData += "</div>";
             songData += "<div class='album'>On the album ";
-              songData += currentSong.album;
+            songData += currentSong.album;
             songData += "</div>";
-          songData += "</div>";
+            songData += "</div>";
         }
 
-        console.log("songData", songData);
         contentEl.html(songData);
-});
-
-  // $.ajax({
-  //   url: "../songs.json"
-  // }).done(function(data) {
-  //   songs = data.songs;
-  //   console.log("data", data);
-  //   $.ajax({
-  //     url: "../songs2.json"
-  //     }).done(function(data2) {
-  //       console.log("data2", data2);
-  //       var songData = "";
-  //       var currentSong;
-        
-  //       data2.songs.forEach(function(song){
-  //         songs.push(song);
-  //       });
-
-  //       console.log("songs", songs);
-        
-  //       for (var i = 0; i < songs.length; i++) {
-  //         currentSong = songs[i];
-
-  //         songData += "<div class='song-block'>";
-  //           songData += "<h1>" + currentSong.title + "</h1>";
-  //           songData += "<div class='artist'>Performed by ";
-  //             songData += currentSong.artist;
-  //           songData += "</div>";
-  //           songData += "<div class='album'>On the album ";
-  //             songData += currentSong.album;
-  //           songData += "</div>";
-  //         songData += "</div>";
-  //       }
-
-  //       console.log("songData", songData);
-  //       contentEl.html(songData);
-  //     }).fail(function(error) {
-  //       console.log( "error" , error);
-  //     });
-  // }).fail(function(error) {
-  //   console.log( "error" , error);
-  // });
+    });
 });
